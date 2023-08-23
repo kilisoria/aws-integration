06afc32b7f5ab9985f3c018359a4f9c639d0019e
Cristians Soria <cristiansoria@gmail.com>
2023-08-23 19:39:17 -0300

    Remove unused projects

--- aws-services/cloud-emulator/appsync/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "appsyncapi": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
